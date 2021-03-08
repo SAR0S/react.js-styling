325d1b19755ca60108e8923ab5a20ec503809835
SAR0S <weorbit79@gmail.com>
2021-03-08 20:58:28 +0900

    Add conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './App.module.css';
+import ConditionalRendering from './components/conditionalRendering/ConditionalRendering'
 
 console.log(styles);
 
 function App() {
   return (
-    <div className={[styles.box, styles.blue].join(' ')}>
-      
+    // <div className={[styles.box, styles.blue].join(' ')}>
+    <div>
+      <ConditionalRendering/>
     </div>
   );
 }
